06ed866e939d19ab33886372108e131b4e98854d
Junaid-khatri <junaidkhatri102@gmail.com>
2024-06-26 16:26:58 +0530

    changed front end code

--- bpm-camunda-application/src/app/services/claim.service.ts
+++ bpm-camunda-application/src/app/services/claim.service.ts
@@ -6,16 +6,30 @@ import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
   providedIn: 'root'
 })
 export class ClaimService {
-   url='http://10.0.0.51:8081/loan/claim'
-   unclaimurl='http://localhost:8081/api1'
+   url='http://localhost:8081/loan/claim';
+   unclaimurl='http://localhost:8081/api1';
+
+   unassignTask = "http://localhost:8081/loan/unassigntask";
+   assignTask = "http://localhost:8081/loan/assigntask";
+
   constructor(private http:HttpClient) { }
 
 claim(data: any): Observable<any> {
     console.log('claim Data Sent:', data); 
     return this.http.post<any>(this.url, data);
   }
+
   unclaim(data:any):Observable<any>
   {
     return this.http.post<any>(this.unclaimurl,data);
   }
+
+  getUnassignTask(data:any):Observable<any>{
+   return this.http.post<any>(this.unassignTask,data);
+  }
+
+  getAssignTask(data:any):Observable<any>{
+    return this.http.post<any>(this.assignTask,data);
+   }
+
 }
---
+++ bpm-camunda-application/src/app/taskDetail.ts
@@ -0,0 +1,6 @@
+export interface TaskDetails{
+    bussinessKey:string,
+    empId:number,
+    existingLoanAmount:number,
+    newLoanAmount:number
+}
